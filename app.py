4edbd0190f25e787ff313697fb67641f010e5aa6
augustinetirkey-gif <augustine.tirkey@bscdsaih.christuniversity.in>
2026-03-13 12:12:06 +0530

    Update app.py

--- app.py
+++ app.py
@@ -138,6 +138,11 @@ if uploaded_file is not None:
     
     st.sidebar.subheader("🔍 Filter Strategy")
     st_year = st.sidebar.multiselect("Fiscal Year", options=sorted(df_master['YEAR'].unique()), default=df_master['YEAR'].unique())
+    st_year = st.sidebar.multiselect("Fiscal Year", options=sorted(df_master['YEAR'].unique()), default=df_master['YEAR'].unique())
+    forecast_year = st.sidebar.multiselect(
+         "Select Forecast Year (AI Prediction)",
+          [None, 2006, 2007, 2008, 2009, 2010]
+    )
     st_country = st.sidebar.multiselect("Active Markets", options=sorted(df_master['COUNTRY'].unique()), default=df_master['COUNTRY'].unique())
     st_product = st.sidebar.multiselect("Product Line", options=sorted(df_master['PRODUCTLINE'].unique()), default=df_master['PRODUCTLINE'].unique())
     
