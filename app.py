9442c1be7415259038f2b09b9b3e555c67a65501
augustinetirkey-gif <augustine.tirkey@bscdsaih.christuniversity.in>
2026-03-13 15:40:53 +0530

    Update app.py

--- app.py
+++ app.py
@@ -365,7 +365,7 @@ if uploaded_file is not None:
                     pred = selected_model.predict(inp)[0]
            
                        
-            st.markdown(f"""
+                st.markdown(f"""
                     <div style='background-color:#e3f2fd;padding:30px;border-radius:15px;text-align:center;border: 2px solid #1f4e79;margin-bottom:25px;'>
                         <p style='color:#1f4e79; font-weight:bold; margin-bottom:0;'>PROJECTED REVENUE</p>
                         <h1 style='color:#1f4e79; font-size:48px; margin-top:0;'>${pred:,.2f}</h1>
